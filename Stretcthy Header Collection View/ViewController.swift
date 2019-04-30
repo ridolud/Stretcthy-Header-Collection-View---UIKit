163dfacc81c875dce81bdb6460e8f72a0b63d97b
ridolud <faridho.lutfi@gmail.com>
2019-05-01 03:07:32 +0700

    Initial Commit

---
+++ Stretcthy Header Collection View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stretcthy Header Collection View
+//
+//  Created by Faridho Luedfi on 01/05/19.
+//  Copyright © 2019 ridolud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
